b7b607e18f05f8a262414373e231b10ec39bf963
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-11-30 21:43:43 -0500

    adding import aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@atoms': './src/components/atoms',
+          '@oranisms': './src/components/organisms',
+          '@molecules': './src/components/molecules',
+          '@styles': './src/styles',
+        },
+      },
+    ],
+  ],
 };
