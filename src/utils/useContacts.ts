f16a34b44ef995a32ca7e146988106363cf31e0b
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-12-08 21:14:03 -0500

    adding batch commiting docs to firebase

--- src/utils/useContacts.ts
+++ src/utils/useContacts.ts
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
 import Contacts, {Contact} from 'react-native-contacts';
+import {firestore} from './config';
 
 export const getAllContacts = async () => {
   try {
@@ -10,7 +11,7 @@ export const getAllContacts = async () => {
   }
 };
 
-export const useContacts = () => {
+export const useContacts = (firebaseUserID: string | undefined) => {
   const [contacts, setContacts] = useState<Contact[] | undefined>();
 
   useEffect(() => {
@@ -18,7 +19,24 @@ export const useContacts = () => {
       const newContacts = await getAllContacts();
       setContacts(newContacts);
     })();
-  }, []);
+  }, [firebaseUserID]);
 
+  // write the contacts to firestore
+  if (contacts && firebaseUserID) {
+    var batch = firestore.batch();
+    console.log('Adding contacts to firestore');
+    contacts.forEach((contact) => {
+      const docRef = firestore
+        .collection('users')
+        .doc(firebaseUserID)
+        .collection('ios_contacts')
+        .doc();
+
+      batch.set(docRef, contact);
+    });
+
+    // commit all the contacts to the collection
+    batch.commit();
+  }
   return contacts;
 };
