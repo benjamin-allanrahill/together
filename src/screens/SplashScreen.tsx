8d5a24fd5f95796e28c9e693b177320a816a5ad2
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-12-09 20:46:57 -0500

    renaming

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const SplashScreen = () => {
+  return (
+    <Text
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 45,
+      }}>
+      together
+    </Text>
+  );
+};
