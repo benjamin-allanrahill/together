b4b407346df3eb0d5db31f7d720e8ebed3ccc92d
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-12-13 11:13:06 -0500

    inital getContactsFromFirestore commit

---
+++ src/utils/Contacts/Firebase/getContactsFromFirestore.ts
@@ -0,0 +1,17 @@
+import {Contact} from 'react-native-contacts';
+import {firestore} from '../../Firebase';
+
+export const getContactsFromFirestore = async (
+  firebaseUserID: string | undefined,
+): Promise<Contact[] | undefined> => {
+  // write the contacts to firestore
+  if (firebaseUserID) {
+    console.log('Getting contacts from firestore');
+    const snapshot = await firestore
+      .collection(`users/${firebaseUserID}/ios_contacts`)
+      .get();
+
+    // return an array with the data using firebase map function
+    return snapshot.docs.map((doc) => doc.data()) as Contact[];
+  }
+};
