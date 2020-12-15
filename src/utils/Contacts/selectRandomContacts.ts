169504eda0a50d73ee6d3a2c20917da61b9830e9
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-12-15 16:47:45 -0500

    fixing if undefined error

--- src/utils/Contacts/selectRandomContacts.ts
+++ src/utils/Contacts/selectRandomContacts.ts
@@ -2,11 +2,16 @@ import {Contact} from 'react-native-contacts';
 
 export const selectRandomContacts = (
   number: number,
-  contacts: Contact[],
+  contacts: Contact[] | undefined,
 ): Contact[] => {
-  var randomContacts: Contact[] = [];
-  for (let i = 0; i < number; i++) {
-    randomContacts.push(contacts[Math.floor(Math.random() * contacts.length)]);
+  if (contacts) {
+    var randomContacts: Contact[] = [];
+    for (let i = 0; i < number; i++) {
+      randomContacts.push(
+        contacts[Math.floor(Math.random() * contacts.length)],
+      );
+    }
+    return randomContacts;
   }
-  return randomContacts;
+  return [];
 };
